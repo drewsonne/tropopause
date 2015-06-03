a88425775646b6a06570a3ec5984a9bb916afb7d
Drew J. Sonne <drew.sonne@nordcloud.com>
2015-06-03 21:51:51 +0100

    Structured tests so that they can be tested.

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,3 +1,4 @@
+import json
 import unittest
 from tropopause.components import compute
 from tropopause.project import Project
@@ -7,7 +8,8 @@ from tropopause.components.db import MySQL
 __author__ = 'drews'
 
 class TestBasic(unittest.TestCase):
-    def test_something(self):
+
+    def setup_project(self):
         test_project = Project(
             title='TestProject',
             zone_name='testproject',
@@ -16,16 +18,35 @@ class TestBasic(unittest.TestCase):
         test_project.region = 'eu-west-1'
         test_project.default_ami = 'ami-123456'
         test_project.default_instance_size = 't2.micro'
+        return test_project
 
+    def test_something(self):
+        cfn_json = self.setup_project()\
+            .generate_troposphere()\
+            .to_json()
+        cfn_dict = json.loads(cfn_json)
+        # MAKE ASSERTIONS!!!
+
+    def test_addcompute(self):
+        test_project = self.setup_project()
         test_project.add_compute_group(compute.Group(
             title='BastionHost',
             private=False
         ))
+        cfn_json = test_project\
+            .generate_troposphere()\
+            .to_json()
+        cfn_dict = json.loads(cfn_json)
+        # MAKE ASSERTIONS!!!
 
+    def test_adddb(self):
+        test_project = self.setup_project()
         test_project.add_database(MySQL())
-
-
-        self.assertEqual(True, False)
+        cfn_json = test_project\
+            .generate_troposphere()\
+            .to_json()
+        cfn_dict = json.loads(cfn_json)
+        # MAKE ASSERTIONS!!!
 
 
 if __name__ == '__main__':
